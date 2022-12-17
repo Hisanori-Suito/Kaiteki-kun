453ab6fb4f8a6c6f72d30dd94979c1e3b1789dcc
Hisanori-Suito <180447039@ccmailg.meijo-u.ac.jp>
2022-12-17 12:27:31 +0900

    First Commit

---
+++ Kaiteki-kun_yolov7/detect.py
@@ -0,0 +1,285 @@
+import argparse
+import time
+from pathlib import Path
+
+import cv2
+import torch
+import torch.backends.cudnn as cudnn
+from numpy import random
+
+from models.experimental import attempt_load
+from utils.datasets import LoadStreams, LoadImages
+from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
+    scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
+from utils.plots import plot_one_box
+from utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
+
+import math
+import requests
+
+def detect(save_img=False):
+    source, weights, view_img, save_txt, imgsz, trace = opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size, not opt.no_trace
+    save_img = not opt.nosave and not source.endswith('.txt')  # save inference images
+    webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
+        ('rtsp://', 'rtmp://', 'http://', 'https://'))
+
+    # Directories
+    save_dir = Path(increment_path(Path(opt.project) / opt.name, exist_ok=opt.exist_ok))  # increment run
+    (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
+
+    # Initialize
+    set_logging()
+    device = select_device(opt.device)
+    half = device.type != 'cpu'  # half precision only supported on CUDA
+
+    # Load model
+    model = attempt_load(weights, map_location=device)  # load FP32 model
+    stride = int(model.stride.max())  # model stride
+    imgsz = check_img_size(imgsz, s=stride)  # check img_size
+
+    if trace:
+        model = TracedModel(model, device, opt.img_size)
+
+    if half:
+        model.half()  # to FP16
+
+    # Second-stage classifier
+    classify = False
+    if classify:
+        modelc = load_classifier(name='resnet101', n=2)  # initialize
+        modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=device)['model']).to(device).eval()
+
+    param = 0
+    judge = 0b00
+    flag = 0
+    crowd_1 = 0
+    crowd_2 = 0
+    lost_1 = 0
+    lost_2 = 0
+    while(1):
+        
+        try:
+            if flag == 0:
+                spr_r = requests.get("http://192.168.1.99/?test="+format(judge, '03b')+"/",timeout=(3.0,7.5))
+                with open(f"{source}/{param:06d}.jpg", "wb") as spr_f:
+                    spr_f.write(spr_r.content)
+            else:
+                spr_r = requests.get("http://192.168.1.99/?test="+format(judge, '03b')+"/",timeout=(6.0,15.0))
+                with open(f"{source}/{param:06d}.jpg", "wb") as spr_f:
+                    spr_f.write(spr_r.content)
+        except:
+            if flag == 0:
+                flag = 1
+            else:
+                flag = 0
+            judge = 0b00
+            continue
+
+        judge = 0b00
+        n_1 = 0
+
+        # Set Dataloader
+        vid_path, vid_writer = None, None
+        if webcam:
+            view_img = check_imshow()
+            cudnn.benchmark = True  # set True to speed up constant image size inference
+            dataset = LoadStreams(source, img_size=imgsz, stride=stride)
+        else:
+            dataset = LoadImages(f"{source}/{param:06d}.jpg", img_size=imgsz, stride=stride)
+
+        # Get names and colors
+        names = model.module.names if hasattr(model, 'module') else model.names
+        colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
+
+        # Run inference
+        if device.type != 'cpu':
+            model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
+        old_img_w = old_img_h = imgsz
+        old_img_b = 1
+
+        t0 = time.time()
+        for path, img, im0s, vid_cap in dataset:
+            img = torch.from_numpy(img).to(device)
+            img = img.half() if half else img.float()  # uint8 to fp16/32
+            img /= 255.0  # 0 - 255 to 0.0 - 1.0
+            if img.ndimension() == 3:
+                img = img.unsqueeze(0)
+
+            # Warmup
+            if device.type != 'cpu' and (old_img_b != img.shape[0] or old_img_h != img.shape[2] or old_img_w != img.shape[3]):
+                old_img_b = img.shape[0]
+                old_img_h = img.shape[2]
+                old_img_w = img.shape[3]
+                for i in range(3):
+                    model(img, augment=opt.augment)[0]
+
+            # Inference
+            t1 = time_synchronized()
+            pred = model(img, augment=opt.augment)[0]
+            t2 = time_synchronized()
+
+            # Apply NMS
+            pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
+            t3 = time_synchronized()
+
+            # Apply Classifier
+            if classify:
+                pred = apply_classifier(pred, modelc, img, im0s)
+
+            label_count = []
+
+            # Process detections
+            for i, det in enumerate(pred):  # detections per image
+                if webcam:  # batch_size >= 1
+                    p, s, im0, frame = path[i], '%g: ' % i, im0s[i].copy(), dataset.count
+                else:
+                    p, s, im0, frame = path, '', im0s, getattr(dataset, 'frame', 0)
+
+                p = Path(p)  # to Path
+                save_path = str(save_dir / p.name)  # img.jpg
+                txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
+                gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+                if len(det):
+                    # Rescale boxes from img_size to im0 size
+                    det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+
+                    # Print results
+                    for c in det[:, -1].unique():
+                        n = (det[:, -1] == c).sum()  # detections per class
+                        s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+
+                    # Write results
+                    for *xyxy, conf, cls in reversed(det):
+                        if save_txt:  # Write to file
+                            xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                            line = (cls, *xywh, conf) if opt.save_conf else (cls, *xywh)  # label format
+                            with open(txt_path + '.txt', 'a') as f:
+                                f.write(('%g ' * len(line)).rstrip() % line + '\n')
+
+                        if save_img or view_img:  # Add bbox to image
+                            label = f'{names[int(cls)]} {conf:.2f}'
+                            label_name = f'{names[int(cls)]}'
+                            dis = plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=1, label_name = label_name, 
+                                            person_x=person_x, person_y=person_y, lost_x=lost_x, lost_y=lost_y)
+                            
+                            # bboxの距離を計算
+                            if label_name == "person" or label_name == "handbag":
+                                label_count.append(label_name)
+                            count_person = label_count.count("person")
+                            count_lost = label_count.count("handbag")
+                            if count_person > 3:
+                                label_count.remove("person")
+                            if count_lost > 3:
+                                label_count.remove("handbag")
+
+                            if dis[0] != 0.0:
+                                person_dis = dis[0]
+                            if dis[1] != 0.0:
+                                lost_dis = dis[1]
+                            if count_person > 0 and count_lost > 0:
+                                distance = math.sqrt((person_dis - lost_dis) * (person_dis - lost_dis))
+                                if distance > 230 :
+                                        cv2.putText(im0, "lost ", (250, 50), 0, 1.0, (71, 99, 255), 3)
+                                        judge = judge + 0b10
+
+                        # Print counter
+                        n_1 = (det[:, -1] == 0).sum() #ラベルAの総数をカウント
+                        a = f"{n_1} "#{'A'}{'s' * (n_1 > 1)}, " 
+                        cv2.putText(im0, "counter " , (20, 50), 0, 1.0, (71, 99, 255), 3)
+                        cv2.putText(im0, "person : " + str(a), (20, 100), 0, 1.0, (71, 99, 255), 3)
+
+                        if flag == 0:
+                            crowd_1 = n_1
+                        else :
+                            crowd_2 = n_1
+
+                    if flag == 0:
+                        if crowd_2 - crowd_1 > 1:
+                            judge = judge + 0b01
+                        else:
+                            judge = judge + 0b00
+                    else:
+                        if crowd_1 - crowd_2 > 1:
+                            judge = judge + 0b01
+                        else:
+                            judge = judge + 0b00
+
+                # Print time (inference + NMS)
+                print(f'{s}Done. ({(1E3 * (t2 - t1)):.1f}ms) Inference, ({(1E3 * (t3 - t2)):.1f}ms) NMS')
+
+                # Stream results
+                if view_img:
+                    cv2.imshow(str(p), im0)
+                    cv2.waitKey(1)  # 1 millisecond
+
+                # Save results (image with detections)
+                if save_img:
+                    if dataset.mode == 'image':
+                        cv2.imwrite(save_path, im0)
+                        print(f" The image with the result is saved in: {save_path}")
+                    else:  # 'video' or 'stream'
+                        if vid_path != save_path:  # new video
+                            vid_path = save_path
+                            if isinstance(vid_writer, cv2.VideoWriter):
+                                vid_writer.release()  # release previous video writer
+                            if vid_cap:  # video
+                                fps = vid_cap.get(cv2.CAP_PROP_FPS)
+                                w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                                h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                            else:  # stream
+                                fps, w, h = 30, im0.shape[1], im0.shape[0]
+                                save_path += '.mp4'
+                            vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
+                        vid_writer.write(im0)
+
+        if save_txt or save_img:
+            s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
+            #print(f"Results saved to {save_dir}{s}")
+
+        print(f'Done. ({time.time() - t0:.3f}s)')
+
+        param = param + 1
+        if flag == 0:
+            flag = 1
+        else:
+            flag = 0
+    
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str, default='yolov7.pt', help='model.pt path(s)')
+    parser.add_argument('--source', type=str, default='inference/images', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--view-img', action='store_true', help='display results')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--update', action='store_true', help='update all models')
+    parser.add_argument('--project', default='runs/detect', help='save results to project/name')
+    parser.add_argument('--name', default='exp', help='save results to project/name')
+    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+    parser.add_argument('--no-trace', action='store_true', help='don`t trace model')
+    opt = parser.parse_args()
+    print(opt)
+    #check_requirements(exclude=('pycocotools', 'thop'))
+    person_x = 0.0
+    person_y = 0.0
+    lost_x = 0.0
+    lost_y = 0.0
+
+
+    with torch.no_grad():
+        if opt.update:  # update all models (to fix SourceChangeWarning)
+            for opt.weights in ['yolov7.pt']:
+                detect()
+                strip_optimizer(opt.weights)
+        else:
+            detect()
---
+++ Kaiteki-kun_yolov7/readme.md
@@ -0,0 +1,41 @@
+# 快適くん-YOLOv7
+
+## IPアドレスの準備
+
+1.  接続するspresenseと繋がっているwifiルータのIPアドレスを取得(例：Linuxではコマンド`ifconfig`)
+2.  取得したアドレスを`detect.py`の63行目`"http://***.***.*.**/?test="`(1台目)、67行目`"http://***.***.*.**/?test="`(2台目)に記入
+    (同じアドレスを記入した場合は1台での動作となる)
+
+## 学習済みのモデルのダウンロード
+
+- 次のコマンドで学習済みのモデルをダウンロード
+    ```
+    wget https://github.com/WongKinYiu/yolov7/releases/download/v0.1/yolov7-e6e.pt﻿
+    ```
+
+## 検証
+
+1.  次のコマンドで検証を開始する
+    ```bash
+    python detect.py --source inference/spresense/ --weights yolov7-e6e.pt --conf 0.25 --img-size 1280 --device 0﻿
+    ```
+
+   - spresenseの画像は`inference/spresense/`内に1台ずつ交互に保存される
+     (保存できない場合はスキップされる)
+   - 物体検出を行った結果の画像は`runs/detect/exp**/`内に保存される
+
+2.  検証が終わったら`ctrl` + `z`(Linuxの場合)などのプログラムを停止させるコマンドで検証を停止させる
+3.  必要があれば`ctrl` + `d`(Linuxの場合)などのプログラムを終了させるコマンドでプログラムの実行を完全に終了させる
+
+## 可変パラメータ
+
+1.  忘れ物検出
+   - `detect.py`の166,169,202行目及び`utils/plots.py`の72行目 `"******"` に記入するラベル名を変えることで忘れ物検出したい物を設定することができる(初期handbag)
+
+2.  混雑検出
+   - `detect.py`の197,202行目 `crowd_1(2) - crowd_2(1) > *` に記入する数値を変えることで警告音を鳴らす人数差を設定することができる(初期値1)
+
+## 補注
+
+- 今回のプログラムの大半はYOLOv7をそのまま使用しているため、必要に応じて公式の[git](https://github.com/WongKinYiu/yolov7)や[論文](https://arxiv.org/abs/2207.02696)を参考にすると良い
+
